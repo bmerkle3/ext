56856de05cd1c1b88b615ad03f564f1c9822290a
Braidy Merkle <braidy.merkle@nordstrom.com>
2021-04-13 00:42:17 -0500

    correct selector

--- taurus.js
+++ taurus.js
@@ -1,14 +1,14 @@
 const url = window.location.href;
 
-var divs = document.getElementsByTagName("bpzDkR ");
+var divs = document.getElementsByTagName("div");
 // var divs = document.querySelectorAll("div");
-
+console.log("right after ", divs);
 const button = document.createElement("button");
 button.textContent = "analyze me";
 button.classList.add("taurus-button");
 button.onclick = function (e) {
   e.stopPropagation();
-  console.log(divs);
+  console.log("in the function", divs);
   if (button.classList.contains("showing-debunk")) {
     button.classList.remove("showing-debunk");
   } else {
