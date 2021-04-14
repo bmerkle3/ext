9b24aafda594ec3476b55c283dc0ae91680df863
Braidy Merkle <braidy.merkle@nordstrom.com>
2021-04-14 17:25:48 -0500

    adds and hides debunk box

--- taurus.js
+++ taurus.js
@@ -1,22 +1,16 @@
 const url = window.location.href;
 
-// var divs = document.querySelectorAll("div");
-
-// var containerFixed = document.getElementsByClassName("l-container-fixed");
-// var containerChild = containerFixed.firstChild;
-// divs[0].firstChild.classList.add("bg-black");
-
-// targetNode.classList.add("bg-black");
-
 const button = document.createElement("button");
-const debunk = document.createElement("div");
-debunk.className = "box-visible";
-debunk.innerHTML = "you are reading me!";
+const debunkBox = document.createElement("div");
+debunkBox.className = "box-visible";
+debunkBox.innerHTML = "you are reading me!";
 
 button.textContent = "analyze me";
-button.classList.add("taurus-button");
+button.classList.add("button-plain");
 
 window.addEventListener("load", function () {
+  // the way i listen for page load isn't good. If i click in from the search results page this doesn't run :(
+  console.log("look, mom! I can load");
   const divs = document.getElementsByTagName("div");
   const targetNode =
     divs[0].firstChild.childNodes[3].childNodes[1].childNodes[1].childNodes[1]
@@ -25,17 +19,17 @@ window.addEventListener("load", function () {
   targetNode.classList.add("bg-black");
   targetNode.appendChild(button);
 
-  console.log("right after ", targetNode);
-
   button.onclick = function (e) {
     e.stopPropagation();
-    console.log("in the function", divs[0][3]);
-    if (button.classList.contains("showing-debunk")) {
-      button.classList.remove("showing-debunk");
+
+    if (button.classList.contains("button-showing-debunk")) {
+      button.classList.remove("button-showing-debunk");
+      button.textContent = "analyze me";
+      targetNode.removeChild(debunkBox);
     } else {
-      button.classList.add("showing-debunk");
+      button.classList.add("button-showing-debunk");
+      targetNode.appendChild(debunkBox);
+      button.textContent = "hide this debunk";
     }
   };
 });
-
-// divs[0].parentElement.appendChild(button);
