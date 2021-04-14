3367f0fee9a6b8673aa1f300071a4bd47b9190c0
Braidy Merkle <braidy.merkle@nordstrom.com>
2021-04-14 18:44:04 -0400

    Dom man (#1)
    * identifies target element by changing bg color, appends analyze button
    
    *button toggles debunk box visibility

--- background.js
+++ background.js
@@ -11,3 +11,17 @@
 //   // button.addEventListener("click", handleButtonClick);
 //   // document.appendChild(button);
 // });
+
+// https://stackoverflow.com/questions/25107774/how-do-i-send-an-http-get-request-from-a-chrome-extension
+
+// console.log("Hello, world!");
+
+// const endpoint =
+//   "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
+
+// const cities = [];
+// fetch(endpoint)
+//   .then((blob) => blob.json())
+//   .then((data) => cities.push(...data));
+
+// console.log("cities", cities);
--- taurus.js
+++ taurus.js
@@ -1,18 +1,35 @@
 const url = window.location.href;
 
-var divs = document.getElementsByTagName("div");
-// var divs = document.querySelectorAll("div");
-console.log("right after ", divs);
 const button = document.createElement("button");
+const debunkBox = document.createElement("div");
+debunkBox.className = "box-visible";
+debunkBox.innerHTML = "you are reading me!";
+
 button.textContent = "analyze me";
-button.classList.add("taurus-button");
-button.onclick = function (e) {
-  e.stopPropagation();
-  console.log("in the function", divs);
-  if (button.classList.contains("showing-debunk")) {
-    button.classList.remove("showing-debunk");
-  } else {
-    button.classList.add("showing-debunk");
-  }
-};
-divs[0].parentElement.appendChild(button);
+button.classList.add("button-plain");
+
+window.addEventListener("load", function () {
+  // the way i listen for page load isn't good. If i click in from the search results page this doesn't run :(
+  console.log("look, mom! I can load");
+  const divs = document.getElementsByTagName("div");
+  const targetNode =
+    divs[0].firstChild.childNodes[3].childNodes[1].childNodes[1].childNodes[1]
+      .childNodes[1];
+
+  targetNode.classList.add("bg-black");
+  targetNode.appendChild(button);
+
+  button.onclick = function (e) {
+    e.stopPropagation();
+
+    if (button.classList.contains("button-showing-debunk")) {
+      button.classList.remove("button-showing-debunk");
+      button.textContent = "analyze me";
+      targetNode.removeChild(debunkBox);
+    } else {
+      button.classList.add("button-showing-debunk");
+      targetNode.appendChild(debunkBox);
+      button.textContent = "hide this debunk";
+    }
+  };
+});
