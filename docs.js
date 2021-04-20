6229355d8c801774145d223ceae32e81894f6b1a
Braidy Merkle <braidy.merkle@nordstrom.com>
2021-04-20 09:54:31 -0500

    add link to api docs

--- docs.js
+++ docs.js
@@ -8,3 +8,10 @@
 
 // can you require npm modules in chrome extension:
 // https://stackoverflow.com/questions/43684452/is-it-possible-to-require-npm-modules-in-a-chrome-extension
+// https://www.youtube.com/watch?v=gkb_x9ZN0Vo
+
+// api key
+// # https://www.youtube.com/watch?v=17UVejOw3zA
+
+// API resource
+// https://rapidapi.com/edamam/api/edamam-food-and-grocery-database
