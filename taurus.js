ec3dbb5ad14a8ca902331f9dea28f7fee5f5f109
Braidy Merkle <braidy.merkle@nordstrom.com>
2021-04-19 22:04:40 -0500

    mama mia, its spaghetti code

--- taurus.js
+++ taurus.js
@@ -1,3 +1,6 @@
+// require("dotenv").config();
+//  need to get package.json and npm install working for this to actually use the dotenv package
+
 const url = window.location.href;
 
 const debunkBox = document.createElement("div");
@@ -14,22 +17,63 @@ const button = document.createElement("button");
 button.textContent = "analyze me";
 button.classList.add("button-plain");
 
-const endpoint = "https://covid19.mathdro.id/api/countries";
+// need to get package.json and npm install working for this to actually use the dotenv package
+let someResponse = [];
+// fetch(
+//   "https://edamam-food-and-grocery-database.p.rapidapi.com/parser?upc=016000123151",
+//   {
+//     method: "GET",
+//     headers: {
+//       "x-rapidapi-key": process.env.API_KEY,
+//       "x-rapidapi-host": "edamam-food-and-grocery-database.p.rapidapi.com",
+//     },
+//   }
+// )
+//   .then((response) => response.json())
+//   .then((dataa) => (someResponse = dataa))
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
-let countries;
+const ul = document.getElementsByTagName("ul");
+const listItems = ul[3].firstChild.childNodes;
+
+// const endpoint = "https://covid19.mathdro.id/api/countries";
+// let countries;
 // use await to time this better.
-const testing = fetch(endpoint)
-  .then((blob) => blob.json())
-  .then((data) => (countries = data));
-console.log("countries", testing);
+// const testing = fetch(endpoint)
+// .then((blob) => blob.json())
+// .then((data) => (countries = data));
+// console.log("countries", testing);
 // too soon here to declare countryName
 
 window.addEventListener("load", function () {
   // the way i listen for page load isn't good. If i click in from the search results page this doesn't run :(
-  console.log("look, mom! I can load");
-  console.log(countries["countries"][0]["name"]);
-  const countryName = countries["countries"][0]["name"];
-  console.log("a", countryName);
+
+  // console.log(countries["countries"][0]["name"]);
+  // console.log(
+  //   "someResponse here:",
+  //   someResponse.hints[0].food.foodContentsLabel
+  // );
+  // const countryName = countries["countries"][0]["name"];
+  // console.log("a", countryName);
+
+  // grab text from the page, match it using regex comparing to keyword list
+  const text = ul[3].firstChild.innerHTML;
+  const keywords = ["Gluten Free", "Heart Healthy", "sugar free"];
+  const matched = [];
+
+  keywords.forEach(textMatcher);
+  function textMatcher(item) {
+    re = new RegExp(item, "gi");
+    find = text.match(re);
+    if (find) {
+      matched.push(find[0]);
+    }
+  }
+  console.log("matched", matched);
+
+  // grab element on page where we want button to show, add elements there
   const divs = document.getElementsByTagName("div");
   const targetNode =
     divs[0].firstChild.childNodes[3].childNodes[1].childNodes[1].childNodes[1]
